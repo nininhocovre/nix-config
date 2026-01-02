34d6611cc44ec9200d678d947ab8878f6d61112f
Nininho <nininhocovre@gmail.com>
2026-01-02 19:27:05 -0300

    add games subvol

--- hosts/sff-nix/drive-mount.nix
+++ hosts/sff-nix/drive-mount.nix
@@ -18,11 +18,11 @@
     options = [ "nofail" "noatime" "compress=zstd:3" "ssd" "discard=async" "space_cache=v2" "autodefrag" ];
   };
 
-  # fileSystems."/mnt/games" = {
-  #     device = "/dev/disk/by-uuid/590733a9-e41b-45f0-9d7a-5ff65d0bc420";
-  #     fsType = "btrfs";
-  #     options = [ "subvol=games" "nofail" "noatime" "compress=zstd:3" "ssd" "discard=async" "space_cache=v2" "autodefrag" ];
-  #   };
+  fileSystems."/mnt/games" = {
+      device = "/dev/disk/by-uuid/145a3fdb-0b59-4001-80f5-1839494583c0";
+      fsType = "btrfs";
+      options = [ "subvol=@games" "nofail" "noatime" "compress=zstd:3" "ssd" "discard=async" "space_cache=v2" "autodefrag" ];
+    };
 
   zramSwap = {
     enable = true;
