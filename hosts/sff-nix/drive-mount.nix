741454926a7dc7d62ff0111d785728e6ec3c2cea
Nininho <nininhocovre@gmail.com>
2026-01-05 18:12:47 -0300

    change dados mount points

--- hosts/sff-nix/drive-mount.nix
+++ hosts/sff-nix/drive-mount.nix
@@ -12,13 +12,13 @@
     options = [ "subvol=@nix" "noatime" "compress=zstd:3" "ssd" "discard=async" "space_cache=v2" "autodefrag"];
   };
 
-  fileSystems."/mnt/dados" = {
+  fileSystems."/mnt/dados_other" = {
     device = "/dev/disk/by-uuid/590733a9-e41b-45f0-9d7a-5ff65d0bc420";
     fsType = "btrfs";
     options = [ "nofail" "noatime" "compress=zstd:3" "ssd" "discard=async" "space_cache=v2" "autodefrag" ];
   };
 
-  fileSystems."/mnt/move_dados" = {
+  fileSystems."/mnt/dados" = {
     device = "/dev/disk/by-uuid/145a3fdb-0b59-4001-80f5-1839494583c0";
     fsType = "btrfs";
     options = [ "subvol=@dados" "nofail" "noatime" "compress=zstd:3" "ssd" "discard=async" "space_cache=v2" "autodefrag" ];
