27918cdfa888d83c93e186caab3c8388822d137c
Nininho <nininhocovre@gmail.com>
2026-01-21 09:35:24 -0300

    upgrade

--- modules/core/packages.nix
+++ modules/core/packages.nix
@@ -4,7 +4,6 @@
   programs = {
     fuse.userAllowOther = true;
     mtr.enable = true;
-    adb.enable = true;
     hyprlock.enable = true;
     gnupg.agent = {
       enable = true;
@@ -68,7 +67,7 @@
     # waypaper # Change wallpaper
     wget # Tool For Fetching Files With Links
     # ytmdl # Tool For Downloading Audio From YouTube
-
+    android-tools
     # devenv
     # devbox
     # shellify
