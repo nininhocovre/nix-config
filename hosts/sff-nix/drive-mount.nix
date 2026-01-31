e443a94b1ac106a3cc7e1814c4bf8884cbceb561
Nininho <nininhocovre@gmail.com>
2026-01-31 08:51:31 -0300

    remove second nvme

--- hosts/sff-nix/drive-mount.nix
+++ hosts/sff-nix/drive-mount.nix
@@ -12,12 +12,6 @@
     options = [ "subvol=@nix" "noatime" "compress=zstd:3" "ssd" "discard=async" "space_cache=v2" "autodefrag"];
   };
 
-  fileSystems."/mnt/dados_other" = {
-    device = "/dev/disk/by-uuid/590733a9-e41b-45f0-9d7a-5ff65d0bc420";
-    fsType = "btrfs";
-    options = [ "nofail" "noatime" "compress=zstd:3" "ssd" "discard=async" "space_cache=v2" "autodefrag" ];
-  };
-
   fileSystems."/mnt/dados" = {
     device = "/dev/disk/by-uuid/145a3fdb-0b59-4001-80f5-1839494583c0";
     fsType = "btrfs";
@@ -34,7 +28,7 @@
     enable = true;
     priority = 100;
     algorithm = "zstd";
-    memoryPercent = 30;
+    memoryPercent = 35;
   };
 
   services.btrfs.autoScrub = {
