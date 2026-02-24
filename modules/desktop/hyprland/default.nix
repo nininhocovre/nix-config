7358048057a64d866354b11b52dc12584385d52f
Nininho <nininhocovre@gmail.com>
2026-02-23 21:05:04 -0300

    hyprland reset keybind

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -332,12 +332,12 @@ in
                 # Window/Session actions
                 "$mainMod, Q, killactive"
                 "$mainMod SHIFT, Q, exec, hyprctl activewindow | grep pid | tr -d 'pid:' | xargs kill" # Quit active window and all open instances
-                "$mainMod, delete, exit" # kill hyperland session
                 "$mainMod, W, togglefloating" # toggle the window on focus to float
                 "$mainMod SHIFT, G, togglegroup" # toggle the window on focus to float
                 "ALT, return, fullscreen" # toggle the window on focus to fullscreen
                 "$mainMod ALT, L, exec, hyprlock" # lock screen
-                "$mainMod, backspace, exec, pkill -x wlogout || wlogout -b 4" # logout menu
+                "$mainMod, backspace, exit" # kill hyperland session
+                "$mainMod, delete, exec, systemctl reboot" # reboot
 
                 # Applications/Programs
                 "$mainMod, Return, exec, $term"
