15e8894c24e252ef3ccf2d688c8d6afeea37eeaa
Nininho <nininhocovre@gmail.com>
2026-05-01 20:57:18 -0300

    syncthing open ports

--- modules/core/syncthing.nix
+++ modules/core/syncthing.nix
@@ -6,8 +6,12 @@ in
   services.syncthing = {
     enable = true;
     openDefaultPorts = true;
+    guiAddress = "0.0.0.0:8384";
+
     user = "${username}";
     # dataDir = "/home/${username}";
     configDir = "/home/${username}/.config/syncthing";
   };
+
+  networking.firewall.allowedTCPPorts = [ 8384 ];
 }
