745ca7f80ad49ca8eb208b568e4a930e658839a1
Nininho <nininhocovre@gmail.com>
2026-07-09 11:01:13 -0300

    add restic backup

---
+++ modules/hosts/nas-scp/backup.nix
@@ -0,0 +1,24 @@
+{
+  flake.modules.nixos.nas-scp = { pkgs, ... }: {
+    environment.systemPackages = with pkgs; [
+      restic
+      backrest
+    ];
+
+    systemd.services.backrest = {
+      description = "Backrest service";
+      wantedBy = [ "multi-user.target" ];
+      requires = [ "network-online.target" ];
+      script = "backrest";
+      path = [ pkgs.backrest ];
+      environment = {
+        BACKREST_PORT = "0.0.0.0:9898";
+      };
+      serviceConfig = {
+        Type = "simple";
+        User = "root";
+        Group = "root";
+      };
+    };
+  };
+}
--- modules/hosts/sff-nix/docker.nix
+++ modules/hosts/sff-nix/docker.nix
@@ -33,6 +33,7 @@
               # "--restart=unless-stopped"
               "--net=compose-network"
             ];
+            pull = "newer";
           };
         };
       };
