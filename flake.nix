7ae268fa783fe0cdbc403774bf39b6466a8d1dc6
Nininho <nininhocovre@gmail.com>
2026-07-19 18:04:04 -0300

    change nixvim

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-26.05";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -11,12 +11,7 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
     nixvim = {
-      url = "github:Sly-Harvey/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    neovim = {
-      url = "github:Sly-Harvey/nvim";
-      flake = false;
+      url = "github:nix-community/nixvim";
     };
     spicetify-nix = {
       url = "github:Gerg-L/spicetify-nix";
--- modules/nixvim.nix
+++ modules/nixvim.nix
@@ -1,38 +1,20 @@
 { inputs, ... }:
 {
   flake.modules.nixos.nixvim = {
-    home-manager.sharedModules = [ inputs.self.modules.homeManager.nixvim ];
-  };
+    imports = [
+      inputs.nixvim.nixosModules.nixvim
+    ];
 
-  flake.modules.homeManager.nixvim =
-    {
-      pkgs,
-      ...
-    }:
-    let
-      terminal = "kitty";
-    in
-    {
-      home.packages = with pkgs; [
-        inputs.nixvim.packages.${stdenv.hostPlatform.system}.default
-      ];
-      xdg.desktopEntries = {
-        "nvim" = {
-          name = "Neovim wrapper";
-          genericName = "Text Editor";
-          comment = "Edit text files";
-          exec = "${pkgs.${terminal}}/bin/${terminal} --class \"nvim-wrapper\" -e nvim %F";
-          icon = "nvim";
-          mimeType = [
-            "text/plain"
-            "text/x-makefile"
-          ];
-          categories = [
-            "Development"
-            "TextEditor"
-          ];
-          terminal = false; # Important: set to false since we're calling kitty directly
-        };
+    programs.nixvim = {
+      enable = true;
+
+      defaultEditor = true;
+      vimAlias = true;
+
+      colorschemes.catppuccin = {
+        enable = true;
+        settings.flavour = "mocha";
       };
     };
+  };
 }
